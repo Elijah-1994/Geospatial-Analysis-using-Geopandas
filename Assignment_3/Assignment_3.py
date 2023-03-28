aae8c3019600264e80622e4f11c896cc53c0a754
Elijah-1994 <elijah.salami94@gmail.com>
2023-03-28 14:30:36 +0100

    Uploading maps

---
+++ Assignment_3/Assignment_3.py
@@ -0,0 +1,50 @@
+#%%
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import contextily as ctx
+import folium
+from folium.plugins import FastMarkerCluster
+import plotly.express as px
+
+# load gpd
+incidents_gpd = gpd.read_file('AssignmentData/incidents_clean.shp')
+
+# plot folium map
+m = folium.Map(location=[39.962334, -75.161446],zoom_start=10)
+m = folium.Map(location=[39.962334, -75.161446], tiles="CartoDB dark_matter", zoom_start=15)
+folium.GeoJson(data= incidents_gpd["geometry"]).add_to(m)
+m
+
+# plot folium cluster map
+m.save("clusterMap.html")
+m = folium.Map(location=[39.962334, -75.161446], tiles='cartodbpositron', zoom_start=10)
+folium.GeoJson(data= incidents_gpd ["geometry"][0]).add_to(m)
+m = folium.Map([39.962334, -75.161446], zoom_start=10, tiles='cartodbpositron')
+folium.GeoJson(incidents_gpd.sample(10)).add_to(m)
+m.save('acled.html')
+incidents_sample = incidents_gpd.sample(500)
+cmap = folium.Map(location=[39.962334, -75.161446],
+                        zoom_start=10,
+                        tiles='CartoDB dark_matter')
+FastMarkerCluster(data=list(zip(incidents_sample.geometry.y.values, incidents_sample.geometry.x.values))).add_to(cmap)
+folium.LayerControl().add_to(cmap)
+cmap
+
+
+
+
+
+
+
+
+
+
+
+# px.scatter_geo(incidents, lat="latitude", lon="longitude")
+# fig = px.scatter_mapbox(
+#     incidents,   
+#     lat="lat", 
+#     lon="lng", 
+#     mapbox_style="carto-darkmatter",
+#     zoom=1)
+# fig.show()
